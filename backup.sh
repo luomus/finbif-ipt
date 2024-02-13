7699a853c6123a4cd410abf636ec87421d1329cf
wkmor1 <willi@mmorris.email>
2024-02-13 16:05:55 +0200

    Fix leading slash

--- backup.sh
+++ backup.sh
@@ -4,6 +4,6 @@ TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 
 tar -czf backup-$TIMESTAMP-$BRANCH.tar.gz -C / srv/ipt/
 
-rclone copy "/backup-$TIMESTAMP-$BRANCH.tar.gz" "default:hy-7088-finbif-ipt"
+rclone copy "backup-$TIMESTAMP-$BRANCH.tar.gz" "default:hy-7088-finbif-ipt"
 
 rm backup-$TIMESTAMP-$BRANCH.tar.gz
