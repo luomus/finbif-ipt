10d10e56ea00ba0eae43e1d90a466272a11e3afe
wkmor1 <willi@mmorris.email>
2024-02-13 15:19:18 +0200

    Update permissions

--- permissions.sh
+++ permissions.sh
@@ -2,3 +2,5 @@
 
 chgrp -R 0 /srv/ipt /var/run /usr/local/tomcat /usr/sbin/cron
 chmod -R g=u /srv/ipt /var/run /usr/local/tomcat /usr/sbin/cron /etc/passwd
+chmod gu+rw /var/run
+chmod gu+s /usr/sbin/cron
