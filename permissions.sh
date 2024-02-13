041358486d09d4e2b3cb8f8e0615eef102ddd849
wkmor1 <willi@mmorris.email>
2024-02-13 15:31:38 +0200

    Add permissions to user home

--- permissions.sh
+++ permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chgrp -R 0 /srv/ipt /var/run /usr/local/tomcat /usr/sbin/cron
-chmod -R g=u /srv/ipt /var/run /usr/local/tomcat /usr/sbin/cron /etc/passwd
+chgrp -R 0 /srv/ipt /var/run /usr/local/tomcat /usr/sbin/cron /home/user
+chmod -R g=u /srv/ipt /var/run /usr/local/tomcat /usr/sbin/cron /home/user /etc/passwd
 chmod gu+rw /var/run
 chmod gu+s /usr/sbin/cron
