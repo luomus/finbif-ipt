90f24175c93b0e86dbe703ac4edd77c73df655cd
wkmor1 <willi@mmorris.email>
2024-02-13 12:36:06 +0200

    Add missing env vars

--- backup.sh
+++ backup.sh
@@ -6,6 +6,6 @@ RCLONE_ENDPOINT=a3s.fi
 RCLONE_ACL=private
 TIMESTAMP=$(date '+%Y%m%d%H%M%S') 
 
-tar -czf "/tmp/backup-$TIMESTAMP.tar.gz" "/srv/ipt"
+tar -czf "/tmp/backup-$TIMESTAMP-$BRANCH.tar.gz" "/srv/ipt"
 
-rclone copy "/tmp/backup-$TIMESTAMP.tar.gz" "default:hy-7088-finbif-ipt"
+rclone copy "/tmp/backup-$TIMESTAMP-$BRANCH.tar.gz" "default:hy-7088-finbif-ipt"
