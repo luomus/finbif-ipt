eda6787e19e265c5bd30b1a86ac1b8d8f301fd80
wkmor1 <willi@mmorris.email>
2024-02-14 10:04:26 +0200

    Add plausible

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "${BRANCH}" == "main" ]; then
+
+echo "<script defer data-domain=\"$HOST\" src=\"https://plausible.io/js/script.js\"></script>" >> $CATALINA_HOME/webapps/ROOT/WEB-INF/pages/inc/header.ftl
+
+fi
+
 echo "user:x:$(id -u):0::/home/user:/sbin/nologin" >> /etc/passwd
 
 service cron start
