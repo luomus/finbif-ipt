44751830a1a9a5dfabf66645428e3a63ecefe3b4
wkmor1 <willi@mmorris.email>
2024-02-23 15:56:42 +0200

    Use absolute paths

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 
 TIMESTAMP=$(date '+%Y%m%d%H%M%S') 
 
-tar -czf backup-$TIMESTAMP-$BRANCH.tar.gz -C / srv/ipt/
+tar -czf /home/user/backup-$TIMESTAMP-$BRANCH.tar.gz -C / srv/ipt/
 
-rclone copy "backup-$TIMESTAMP-$BRANCH.tar.gz" "default:hy-7088-finbif-ipt"
+rclone copy "/home/user/backup-$TIMESTAMP-$BRANCH.tar.gz" "default:hy-7088-finbif-ipt"
 
-rm backup-$TIMESTAMP-$BRANCH.tar.gz
+rm /home/user/backup-$TIMESTAMP-$BRANCH.tar.gz
