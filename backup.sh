2a7770d36278d1fec16f61ead82129a4f8479114
wkmor1 <willi@mmorris.email>
2024-03-05 09:31:36 +0200

    Monitor cronjob

--- backup.sh
+++ backup.sh
@@ -2,8 +2,14 @@
 
 TIMESTAMP=$(date '+%Y%m%d%H%M%S') 
 
+echo "Archiving data [$TIMESTAMP]\n"
+
 tar -czf /home/user/backup-$TIMESTAMP-$BRANCH.tar.gz -C / srv/ipt/
 
+echo "Copying data to object store [$TIMESTAMP]\n"
+
 rclone copy "/home/user/backup-$TIMESTAMP-$BRANCH.tar.gz" "default:hy-7088-finbif-ipt"
+
+echo "Removing local archive [$TIMESTAMP]\n"
 
 rm /home/user/backup-$TIMESTAMP-$BRANCH.tar.gz
